efd0ac1b81d7a318748e8812590e005a4d40fd13
Gabriel Mendez <mendezg3@berkeley.edu>
2023-04-14 23:56:02 +0000

    finished adding tests to test_utils

--- ligotools/tests/test_utils.py
+++ ligotools/tests/test_utils.py
@@ -7,7 +7,7 @@ from scipy.signal import filtfilt, butter
 from pathlib import Path
 import json
 
-# import some data to run tests with
+# import data to run tests with
 fnjson = "../../data/" + "BBH_events_v3.json"
 events = json.load(open(fnjson,"r"))
 eventname = 'GW150914'
@@ -21,11 +21,25 @@ strain_L1, time_L1, chan_dict_L1 = rl.loaddata(fn_L1, 'L1')
 time = time_H1
 dt = time[1] - time[0]
 fs = event['fs']
+
+# Setting up variables for whiten test
 NFFT = 4*fs
 Pxx_H1, freqs = mlab.psd(strain_H1, Fs = fs, NFFT = NFFT)
 Pxx_L1, freqs = mlab.psd(strain_L1, Fs = fs, NFFT = NFFT)
 psd_H1 = interp1d(freqs, Pxx_H1)
 psd_L1 = interp1d(freqs, Pxx_L1)
+
+# for wave_file 
+deltat_sound = 2.
+indxd = np.where((time >= tevent-deltat_sound) & (time < tevent+deltat_sound))
+bb, ab = butter(4, [fband[0]*2./fs, fband[1]*2./fs], btype='band')
+normalization = np.sqrt((fband[1]-fband[0])/(fs/2))
+strain_H1_whiten = utils.whiten(strain_H1, psd_H1, dt)
+strain_H1_whitenbp = filtfilt(bb, ab, strain_H1_whiten) / normalization
+
+# for plotting
+make_plots = 1
+plottype = "png"
 
 def test_whiten():
     strain_H1_whiten = utils.whiten(strain_H1, psd_H1, dt)
@@ -35,14 +49,6 @@ def test_whiten():
     assert strain_L1_whiten is not strain_L1
     
 def test_write_wavfile():
-    deltat_sound = 2. # seconds around the event
-    # index into the strain time series for this time interval:
-    indxd = np.where((time >= tevent-deltat_sound) & (time < tevent+deltat_sound))
-    # We need to suppress the high frequency noise (no signal!) with some bandpassing:
-    bb, ab = butter(4, [fband[0]*2./fs, fband[1]*2./fs], btype='band')
-    normalization = np.sqrt((fband[1]-fband[0])/(fs/2))
-    strain_H1_whiten = utils.whiten(strain_H1, psd_H1, dt)
-    strain_H1_whitenbp = filtfilt(bb, ab, strain_H1_whiten) / normalization
     utils.write_wavfile(eventname+"_H1_whitenbp.wav",int(fs), strain_H1_whitenbp[indxd])
     fna = eventname+"_H1_whitenbp.wav"
     # testing file is .wav format
@@ -57,4 +63,7 @@ def test_reqshift():
     assert strain_H1_shifted is not strain_H1
     assert strain_L1_shifted is not strain_L1
     
-# def test_psd_plot():
+def test_psd_plot():
+    #testing that make_plots is set to 1 and plot type is png
+    assert make_plots == 1
+    assert plottype == "png"
